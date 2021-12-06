6d0941618091f9edb5d6a14b7edf4b184e602874
Arthur Oliveira <arthuroliveira.jobs@gmail.com>
2021-12-06 19:03:36 -0300

    primeiro commit

--- 1º EE - P1 - 2021.1/1ee_2021.1_q3.py
+++ 1º EE - P1 - 2021.1/1ee_2021.1_q3.py
@@ -1,30 +1,34 @@
 tabela = {}
 qtd = 0
 
-cpf = int(input("Digite o CPF do funcionário: "))
-while len(cpf) != 11 and cpf != 0:
-    cpf = int(input("Digite um CPF válido, com 11 dígitos: "))
+cpf = input("Digite o CPF do funcionário: ")
+while len(cpf) != 11:
+    cpf = input("Digite um CPF válido, com 11 dígitos: ")
+cpf = int(cpf)
 
-while (cpf != 0) and (qtd < 150):
+while (cpf != '0') and (qtd < 150):
     qtd += 1
     nome = input("Digite o nome do funcionário: ")
     genero = input("Digite o gênero do funcionário (M ou F): ")
-    while (genero != "M") or (genero != "F"):
-        genero = input("Digite a sigla correta (M ou F):")
+    while (genero != 'M') and (genero != 'F'):
+        genero = input("Digite a sigla correta (M ou F): ")
     numDep = int(input("Digite o número de dependentes: "))
     while numDep < 0:
         numDep = int(input("Valor inválido! Digite o número de dependentes: "))
     tabela[cpf] = (nome, genero, numDep)
 
-    cpf = int(input("Digite o CPF do próximo funcionário ou 0 para encerrar: ")
-    while (cpf != 0) and (len(cpf) != 11):
-        cpf = int(input("Digite um CPF válido, com 11 dígitos, ou 0 para encerrar: "))
+    cpf = input("Digite o CPF do próximo funcionário ou 0 para encerrar: ")
+    while len(cpf) != 11 and cpf != '0' and cpf != tabela[qtd-1]:
+        cpf = input("Digite um CPF válido, com 11 dígitos, ou 0 para encerrar: ")
+        if cpf == '0':
+            break
+    cpf = int(cpf)
 
 if qtd == 150:
     print("Número máximo de funcionários atingido.")
 
 entradaGenero = input("Digite o gênero (M ou F) desejado: ")
-while (entradaGenero != "M") or (genero != "F"):
+while (entradaGenero != 'M') and (entradaGenero != 'F'):
     genero = input("Digite a sigla correta (M ou F):")
 entradaNumDep = int(input("Digite o número de dependentes desejado: "))
 while entradaNumDep < 0:
@@ -36,7 +40,7 @@ for cpf in tabela:
     if tabela[cpf][2] == entradaGenero and tabela[cpf][3] >= entradaNumDep:
         qtdSel += 1
         print(tabela[cpf][1], tabela[cpf][3])
-    if (tabela[cpf][3] >= 2*entradaNumDep):
+    if tabela[cpf][3] >= 2*entradaNumDep:
         qtdDob += 1
 
 print(f"A quantidade de pessoas selecionadas foi: {qtdSel}.\n"
